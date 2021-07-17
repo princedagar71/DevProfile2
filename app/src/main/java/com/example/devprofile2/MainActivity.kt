a07e3370422d67281d08a623842376427dd761c0
Prince <princedagar71@gmail.com>
2021-07-17 09:58:36 +0530

    Initial commit

---
+++ app/src/main/java/com/example/devprofile2/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.devprofile2
+
+import android.graphics.BitmapFactory
+import android.media.tv.TvContract
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
+
+//import com.example.devprofile2.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    //private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       // binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.activity_main)
+
+        val bitmap=BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
+        val rounded =RoundedBitmapDrawableFactory.create(resources,bitmap)
+        rounded.cornerRadius=15f
+        //rounded.isCircular=true
+        Logo.setImageDrawable(rounded)
+
+
+
+    }
+}
