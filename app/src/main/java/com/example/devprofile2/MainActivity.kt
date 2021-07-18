6ae38db04ec56edc5f78031d2d4d0d83f56dafd8
Prince <princedagar71@gmail.com>
2021-07-18 16:03:54 +0530

    Project Abandoned

--- app/src/main/java/com/example/devprofile2/MainActivity.kt
+++ app/src/main/java/com/example/devprofile2/MainActivity.kt
@@ -5,6 +5,7 @@ import android.media.tv.TvContract
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import com.example.devprofile2.R.id.Logo
 import kotlinx.android.synthetic.main.activity_main.*
 
 //import com.example.devprofile2.databinding.ActivityMainBinding
@@ -16,11 +17,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
        // binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
-
+        //Logo=findViewById(R.id.Logo)
         val bitmap=BitmapFactory.decodeResource(resources,R.drawable.devslopesprofilelogo)
         val rounded =RoundedBitmapDrawableFactory.create(resources,bitmap)
         rounded.cornerRadius=15f
         //rounded.isCircular=true
+
         Logo.setImageDrawable(rounded)
 
 
